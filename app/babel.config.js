435a4d34acb628112f76980cd1f9c6b70eedcaa1
Quoc Toan <toan124677@gmail.com>
2023-03-07 00:48:34 +0700

    Refactor and packages install

---
+++ app/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-module-resolver',
+        {
+          root: ['./app'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './app/components',
+            '@screens': './app/screens',
+            '@assets': './app/assets',
+            '@constants': './app/constants',
+            '@utils': './app/utils',
+            '@types': './app/types',
+            '@hooks': './app/hooks',
+          },
+        },
+      ],
+    ],
+  }
+}
