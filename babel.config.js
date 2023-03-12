94ad61dda44d7d50c548463c44258377936aa654
Quoc Toan <toan124677@gmail.com>
2023-03-13 00:45:14 +0700

    Testing image

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
           alias: {
             '@components': './app/components',
             '@screens': './app/screens',
-            '@assets': './app/assets',
+            '@assets': './assets',
             '@constants': './app/constants',
             '@utils': './app/utils',
             '@types': './app/types',
