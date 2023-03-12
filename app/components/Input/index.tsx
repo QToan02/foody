be310ef7c2239c150dd58199939b34918f3ddf15
Quoc Toan <toan124677@gmail.com>
2023-03-13 00:49:06 +0700

    Add icon element and convert Text to CText

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -1,27 +1,31 @@
 import { memo } from 'react'
-import { StyleSheet, Text, TextInput, TextInputProps, TextStyle, View } from 'react-native'
+import { Pressable, StyleSheet, TextInput, TextInputProps, View } from 'react-native'
 import { Controller, FieldError } from 'react-hook-form'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { UseControllerProps, RegisterOptions } from 'react-hook-form/dist/types'
+import CText from '@components/CText'
+import { COLORS } from '@constants'
 
 interface Props extends TextInputProps, UseControllerProps {
   label?: string
-  labelStyle?: TextStyle
   error?: FieldError | undefined
   defaultValue?: string
   rules?: RegisterOptions
+  icon?: keyof typeof MaterialCommunityIcons.glyphMap
+  onShowPassword?: () => void
 }
 
-const Input = ({ name, control, rules = {}, label, labelStyle, secureTextEntry, placeholder }: Props) => {
+const Input = ({ name, control, rules = {}, label, secureTextEntry, placeholder, onShowPassword, icon }: Props) => {
   return (
     <View style={styles.container}>
-      {label && <Text style={[labelStyle]}>{label}</Text>}
+      {label && <CText customStyle={styles.label} content={label} />}
       <Controller
         rules={rules}
         control={control}
         name={name}
         render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
           <>
-            <View style={[styles.inputContainer, { borderColor: error ? 'red' : '#e8e8e8' }]}>
+            <View style={[styles.inputContainer, { borderColor: error ? 'red' : COLORS.GRAY }]}>
               <TextInput
                 value={value}
                 placeholder={placeholder}
@@ -29,8 +33,11 @@ const Input = ({ name, control, rules = {}, label, labelStyle, secureTextEntry, 
                 onBlur={onBlur}
                 secureTextEntry={secureTextEntry}
               />
+              <Pressable onPress={onShowPassword}>
+                <MaterialCommunityIcons name={icon} size={22} color={COLORS.ICON_GRAY} />
+              </Pressable>
             </View>
-            {error && <Text style={styles.errorMessage}>{error && error.message}</Text>}
+            {error && <CText customStyle={styles.errorMessage} content={error && error.message} />}
           </>
         )}
       />
@@ -42,19 +49,25 @@ export default memo(Input)
 
 const styles = StyleSheet.create({
   container: {
-    padding: 5,
+    marginTop: 30,
+  },
+  label: {
+    marginBottom: 12,
   },
   inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: 'white',
     width: '100%',
-    borderColor: '#e8e8e8',
+    borderColor: COLORS.GRAY,
     borderWidth: 1,
     borderRadius: 5,
-    paddingHorizontal: 10,
-    marginVertical: 2,
+    padding: 10,
   },
   errorMessage: {
     color: 'red',
+    fontWeight: '600',
     alignSelf: 'stretch',
   },
 })
