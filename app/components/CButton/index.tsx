1a4f3673466df5ccab2538179d012684a21f6219
Quoc Toan <toan124677@gmail.com>
2023-03-13 00:46:47 +0700

    Create custom text and button component

---
+++ app/components/CButton/index.tsx
@@ -0,0 +1,56 @@
+import { memo } from 'react'
+import { Pressable, StyleSheet, ViewStyle } from 'react-native'
+import CText from '@components/CText'
+import { COLORS } from '@constants'
+
+interface Props {
+  title: string
+  onPress: () => void
+  customStyle?: ViewStyle
+  hitSlop?: number
+  variant?: 'primary' | 'secondary'
+}
+
+const CButton = ({ title, onPress, hitSlop, variant = 'primary' }: Props) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      hitSlop={hitSlop}
+      style={variant === 'primary' ? [styles.button, styles.primary] : [styles.button, styles.secondary]}
+    >
+      <CText
+        customStyle={variant === 'primary' ? [styles.content] : [styles.content, styles.contentBlack]}
+        content={title}
+      />
+    </Pressable>
+  )
+}
+
+export default memo(CButton)
+
+const styles = StyleSheet.create({
+  button: {
+    marginVertical: 32,
+    borderRadius: 40,
+    borderWidth: 2,
+    borderColor: COLORS.ORANGE,
+  },
+  primary: {
+    backgroundColor: COLORS.ORANGE,
+  },
+  secondary: {
+    backgroundColor: COLORS.WHITE,
+  },
+  content: {
+    textTransform: 'uppercase',
+    fontWeight: '600',
+    fontSize: 15,
+    color: COLORS.WHITE,
+    textAlign: 'center',
+    paddingVertical: 25,
+    paddingHorizontal: 75,
+  },
+  contentBlack: {
+    color: COLORS.BLACK,
+  },
+})
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Input } from './Input'
+export { default as CText } from './CText'
+export { default as CButton } from './CButton'
